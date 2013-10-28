af78309b5ec58e9fbaa5045654b15af14cb6b768
Tom Vincent <git@tlvince.com>
2013-10-28 00:35:34 +0000

    Added init.sh

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Initialise dotfiles.
+# © 2013 Tom Vincent <http://tlvince.com/contact>
+
+_have() { command -v "$1" >/dev/null 2>&1; }
+_info() { echo "$0: $1"; }
+_error() { _info "$1"; exit 1; }
+_usage() { echo "usage: $0 [package  ...]"; exit; }
+_prompt() { read -p "$1 [y/n] "; [ "$REPLY" == "y" ] || exit 1; }
+
+[ "$1" == "-h" ] || [ "$1" == "--help" ] &&  _usage
+
+_have "stow" || _error "Requires 'stow'"
+[ "$DOTFILES" ] || _error "Set DOTFILES to the dotfiles path"
+
+packages="$@"
+[ "$packages" ] || packages=$(echo $DOTFILES/*/ | sed "s_$DOTFILES/__g")
+
+ignores='^.git(modules|ignore)$|readme.m(d|kd)|.DS_Store|init.sh|.brew|.cask'
+stow="stow --verbose --dir="$DOTFILES" --target="$HOME" --ignore="$ignores""
+
+$stow --simulate $packages
+_prompt "Looks okay?"
+$stow $packages
